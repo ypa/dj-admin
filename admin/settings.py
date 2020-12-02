0883b890797ddc01b6b7b47bd103aefe2c1f8b9a
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-01 19:44:47 -0800

    Update settings

--- admin/settings.py
+++ admin/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_admin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'admin_db',
+        'PORT': '3306'
     }
 }
 
