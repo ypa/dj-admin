bce4aa0b78952b774cd901acc48299d2fe31453f
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-09 20:25:26 -0800

    Add register view and UserSerializer

--- users/views.py
+++ users/views.py
@@ -1,9 +1,24 @@
+from rest_framework import exceptions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
+from .serializers import UserSerializer
+
+
+@api_view(["POST"])
+def register(request):
+    data = request.data
+
+    if data["password"] != data["password_confirm"]:
+        raise exceptions.APIException("Passwords do not match")
+
+    serializer = UserSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
 def users(request):
-    users = User.objects.all()
-    return Response(users)
+    serializer = UserSerializer(User.objects.all(), many=True)
+    return Response(serializer.data)
