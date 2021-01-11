37bb4df2c77523b04e3826cc17bf96111a19b818
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-10 19:32:23 -0800

    Generate JWT and return it when a user logs in

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import exceptions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .authentication import generate_access_token
 from .models import User
 from .serializers import UserSerializer
 
@@ -31,7 +33,12 @@ def login(request):
     if not user.check_password(password):
         raise exceptions.AuthenticationFailed("Incorrect password!")
 
-    return Response("success")
+    response = Response()
+    token = generate_access_token(user)
+    response.set_cookie(key="jwt", value=token, httponly=True)
+    response.data = {"jwt": token}
+
+    return response
 
 
 @api_view(["GET"])
