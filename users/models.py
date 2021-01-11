a78232629d11854492e406599c439280421585a3
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-10 19:01:09 -0800

    Update User model to subclass auth user model and use that as AUTH_USER_MODEL

--- users/models.py
+++ users/models.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
+class User(AbstractUser):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200, unique=True)
     password = models.CharField(max_length=200)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
