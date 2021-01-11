3517a91e7f81c007a4a850c068c28eab7827cadb
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-10 19:19:57 -0800

    Add login view

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import users, register
+from .views import users, register, login
 
 
 urlpatterns = [
     path("users", users),
     path("register", register),
+    path("login", login),
 ]
--- users/views.py
+++ users/views.py
@@ -18,6 +18,22 @@ def register(request):
     return Response(serializer.data)
 
 
+@api_view(["POST"])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    user = User.objects.filter(email=email).first()
+
+    if user is None:
+        raise exceptions.AuthenticationFailed("User not found!")
+
+    if not user.check_password(password):
+        raise exceptions.AuthenticationFailed("Incorrect password!")
+
+    return Response("success")
+
+
 @api_view(["GET"])
 def users(request):
     serializer = UserSerializer(User.objects.all(), many=True)
