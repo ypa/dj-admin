50eca8e6c1934728eaade59d3134139137861ef7
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-11 19:57:34 -0800

    Remove users view

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import users, register, login, AuthenticatedUser, logout
+from .views import register, login, AuthenticatedUser, logout
 
 
 urlpatterns = [
