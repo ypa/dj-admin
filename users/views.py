394cdb7f80cf469691f515a0e89f328fe50bbf66
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-11 19:21:02 -0800

    Add logout view

--- users/views.py
+++ users/views.py
@@ -43,6 +43,15 @@ def login(request):
     return response
 
 
+@api_view(["POST"])
+def logout(_):
+    response = Response()
+    response.delete_cookie(key="jwt")
+    response.data = {"message": "Success"}
+
+    return response
+
+
 class AuthenticatedUser(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
