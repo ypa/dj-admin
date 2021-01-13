c29f6d2065022090f0e0808d3ebbb6826bc517a9
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-12 19:13:37 -0800

    Add RoleViewSet method stubs and wire them up in url routes

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import register, login, AuthenticatedUser, logout, PermissionAPIView
+from .views import (
+    register,
+    login,
+    AuthenticatedUser,
+    logout,
+    PermissionAPIView,
+    RoleViewSet,
+)
 
 
 urlpatterns = [
@@ -9,4 +16,23 @@ urlpatterns = [
     path("logout", logout),
     path("user", AuthenticatedUser.as_view()),
     path("permissions", PermissionAPIView.as_view()),
+    path(
+        "roles",
+        RoleViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        "roles/<str:pk>",
+        RoleViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "delete": "destroy",
+            }
+        ),
+    ),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import exceptions
+from rest_framework import exceptions, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -70,3 +70,23 @@ class PermissionAPIView(APIView):
         serializer = PermissionSerializer(Permission.objects.all(), many=True)
 
         return Response({"data": serializer.data})
+
+
+class RoleViewSet(viewsets.ViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
