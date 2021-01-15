aaa50b959a81009af9d394a96c3b39be8fb0e82e
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-14 19:40:17 -0800

    Add role field as related field in UserSerialzier

--- users/serializers.py
+++ users/serializers.py
@@ -33,10 +33,20 @@ class RoleSerializer(serializers.ModelSerializer):
         return instance
 
 
+class RoleRelatedField(serializers.RelatedField):
+    def to_representation(self, instance):
+        return RoleSerializer(instance).data
+
+    def to_internal_value(self, data):
+        return self.queryset.get(pk=data)
+
+
 class UserSerializer(serializers.ModelSerializer):
+    role = RoleRelatedField(many=False, queryset=Role.objects.all())
+
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "email", "password"]
+        fields = ["id", "first_name", "last_name", "email", "password", "role"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
