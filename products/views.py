ab0483c7091d2a0ea86bfa12b2384252f3436203
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-15 20:11:44 -0800

    Add Generics API views for Product model and wire them up to url routes

--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from admin.pagination import CustomPagination
+from users.authentication import JWTAuthentication
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductGenericAPIView(
+    generics.GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = CustomPagination
+
+    def get(self, request, pk=None):
+        if pk:
+            return Response({"data": self.retrieve(request, pk).data})
+
+        return self.list(request)
+
+    def post(self, request):
+        return Response({"data": self.create(request).data})
+
+    def put(self, request, pk=None):
+        return Response({"data": self.partial_update(request, pk).data})
+
+    def delete(self, request, pk=None):
+        return self.destroy(request, pk)
