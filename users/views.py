397d7e2be52dfdde7b5be5d19e689f39bcb9e359
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-15 19:42:00 -0800

    Pull out permissions to top level of user data response

--- users/views.py
+++ users/views.py
@@ -58,9 +58,10 @@ class AuthenticatedUser(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        serializer = UserSerializer(request.user)
+        data = UserSerializer(request.user).data
+        data["permissions"] = [p["name"] for p in data["role"]["permissions"]]
 
-        return Response({"data": serializer.data})
+        return Response({"data": data})
 
 
 class PermissionAPIView(APIView):
