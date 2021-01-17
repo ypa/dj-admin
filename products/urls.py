1ffc617c3570c20509282d041008be4e635b172d
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-16 19:52:48 -0800

    Add FileUploadVew to products

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProductGenericAPIView
+from .views import ProductGenericAPIView, FileUploadView
 
 
 urlpatterns = [
     path("products", ProductGenericAPIView.as_view()),
     path("products/<str:pk>", ProductGenericAPIView.as_view()),
+    path("upload", FileUploadView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
+from django.core.files.storage import default_storage
 from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
 
 from admin.pagination import CustomPagination
 from users.authentication import JWTAuthentication
@@ -36,3 +39,16 @@ class ProductGenericAPIView(
 
     def delete(self, request, pk=None):
         return self.destroy(request, pk)
+
+
+class FileUploadView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    parser_classes = (MultiPartParser,)
+
+    def post(self, request):
+        file = request.FILES["image"]
+        file_name = default_storage.save(file.name, file)
+        url = default_storage.url(file_name)
+
+        return Response({"url": url})
