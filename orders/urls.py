911e09627a13229976e6dd1a602c5d4f6c78dbb9
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-17 19:39:49 -0800

    Add Orders List and Retrive api views

---
+++ orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import OrderGenericAPIView
+
+urlpatterns = [
+    path("orders", OrderGenericAPIView.as_view()),
+    path("orders/<str:pk>", OrderGenericAPIView.as_view()),
+]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
 
-# Create your views here.
+from admin.pagination import CustomPagination
+from users.authentication import JWTAuthentication
+from .models import Order
+from .serializers import OrderSerializer
+
+
+class OrderGenericAPIView(
+    generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin
+):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    pagination_class = CustomPagination
+
+    def get(self, request, pk=None):
+        if pk:
+            return Response({"data": self.retrieve(request, pk).data})
+
+        return self.list(request)
